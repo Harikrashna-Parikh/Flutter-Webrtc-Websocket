c25bb6c1e7d7924d21757f716f56e48831a23b4e
Harikrashna <hparikh@argusoft.com>
2024-08-22 16:40:45 +0530

    tracks send and recieve part done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:convert';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:videoconferencing/peer_service.dart';
@@ -13,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -38,61 +35,92 @@ class _HomePageState extends State<HomePage> {
   TextEditingController textEditingController = TextEditingController();
   final PeerService peerServiceInstance = PeerService();
   late WebSocketChannel channel;
-  String recieved = "Waiting ====???????";
+  String recieved = "Waiting for connection...";
   final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
 
   @override
   void initState() {
-    tryPeer();
-    // peerServiceInstance.initializePeer();
+    super.initState();
+    initCamera();
     connectSocket();
-    channel.stream.listen((message) {
-      messageReceived(message);
-
-    });
+    peerServiceInstance.initializePeer();
+    peerServiceInstance.peer?.onAddStream = (MediaStream stream) {
+      setState(() {
+        _remoteRenderer.srcObject = stream;
+      });
+    };
   }
 
-  tryPeer() async {
-    await peerServiceInstance.initializePeer();
-    print("peer=========>>>>.. $peerServiceInstance");
+  @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    peerServiceInstance.closePeer();
+    super.dispose();
   }
 
-  connectSocket() async {
-    final url = Uri.parse(
-        'ws://192.1.150.112:8082/api/videochat/efd6fea9-fa56-4c01-bbe4-0853e40c1866w');
-    channel = WebSocketChannel.connect(url);
-    print("=======================>>>>>>>>>>>>>>>> channel getting ready");
-    await channel.ready;
-    print("=======================>>>>>>>>>>>>>>>>  ready , ok!!");
-    print("=======================>>>>>>>>>>>>>>>> $channel");
-  }
-
-  void initCamera() async {
-
+  Future<void> initCamera() async {
+    // Initialize the local renderer
     await _localRenderer.initialize();
+
+    // Get the local media stream (video + audio)
     MediaStream stream = await navigator.mediaDevices.getUserMedia({
       'video': true,
       'audio': true,
     });
 
+    // Set the local renderer's stream once it is initialized
     setState(() {
       _localRenderer.srcObject = stream;
     });
+
+    // Add tracks from the stream to the peer connection
+    stream.getTracks().forEach((track) {
+      peerServiceInstance.peer?.addTrack(track, stream);
+    });
+
+    // Set up the remote stream renderer
+    peerServiceInstance.peer?.onTrack = (RTCTrackEvent event) async {
+      if (event.streams.isNotEmpty) {
+        await _remoteRenderer.initialize();
+        setState(() {
+          _remoteRenderer.srcObject = event.streams[0];
+        });
+      }
+    };
   }
 
-  void messageReceived(dynamic message){
-    print("=======================>>>>>>>>>>>>>>>> message recieved");
-    setState(() {
-      recieved = message;
+  void connectSocket() async {
+    final url = Uri.parse('ws://192.1.150.112:8082/api/videochat/efd6fea9-fa56-4c01-bbe4-0853e40c1866');
+    channel = WebSocketChannel.connect(url);
+
+    channel.stream.listen((message) {
+      handleSignalingMessage(message);
     });
-    print(message);
-    print("=======================>>>>>>>>>>>>>>>> ok!!!");
+  }
 
-    Map<String, dynamic> decodedMessage= jsonDecode(message) as Map<String, dynamic>;
-    if(message.type == "offer"){
-      peerServiceInstance.getAnswer(decodedMessage as RTCSessionDescription);
+  void handleSignalingMessage(dynamic message) async {
+    Map<String, dynamic> decodedMessage = jsonDecode(message);
+
+    if (decodedMessage['type'] == "offer") {
+      final RTCSessionDescription offer = RTCSessionDescription(decodedMessage["sdp"], decodedMessage["type"]);
+      final RTCSessionDescription? answer = await peerServiceInstance.getAnswer(offer);
+      channel.sink.add(jsonEncode(answer?.toMap()));
+
+      while (peerServiceInstance.getIceCandidate() != null) {
+        final IceCandidateWrapper localIceCandidate = IceCandidateWrapper(candidate: peerServiceInstance.localIceCandidate);
+        channel.sink.add(jsonEncode(localIceCandidate.toMap()));
+        break;
+      }
+    } else if (decodedMessage['type'] == "answer") {
+      final RTCSessionDescription answer = RTCSessionDescription(decodedMessage["sdp"], decodedMessage["type"]);
+      peerServiceInstance.peer?.setRemoteDescription(answer);
+    } else if (decodedMessage['type'] == "candidate") {
+      final candidate = RTCIceCandidate(decodedMessage['candidate']['candidate'],
+          decodedMessage['candidate']['sdpMid'], decodedMessage['candidate']['sdpMLineIndex']);
+      peerServiceInstance.peer?.addCandidate(candidate);
     }
-
   }
 
   @override
@@ -100,10 +128,8 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          print("=====================");
-          print(textEditingController.text);
-          channel.sink.add(textEditingController.text);
-          print("=====================");
+          final text = textEditingController.text;
+          channel.sink.add(text);
         },
         child: Icon(Icons.send),
       ),
@@ -112,31 +138,28 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: [
-          Text("hello"),
           TextField(
             controller: textEditingController,
+            decoration: InputDecoration(labelText: 'Send a message'),
           ),
           Text(recieved),
-          SizedBox(
-            height: 12,
-          ),
+          SizedBox(height: 12),
           ElevatedButton(
-              onPressed: () async{
-                initCamera();
-                final offer = await peerServiceInstance.getOffer();
-                channel.sink.add(offer?.toMap().toString());
-                // while(peerServiceInstance.localIceCandidate!=null){
-                print(peerServiceInstance.localIceCandidate);
-                while(peerServiceInstance.getIceCandidate() != null){
-                  channel.sink. add(peerServiceInstance.localIceCandidate?.toMap().toString());
-                  break;
-                }
+            onPressed: () async {
+              initCamera();
+              final offer = await peerServiceInstance.getOffer();
+              channel.sink.add(jsonEncode(offer?.toMap()));
 
-                // }
-
-              },
-              child: Text("Start Video call")),
-        Expanded(child: RTCVideoView(_localRenderer))
+              while (peerServiceInstance.getIceCandidate() != null) {
+                final IceCandidateWrapper localIceCandidate = IceCandidateWrapper(candidate: peerServiceInstance.localIceCandidate);
+                channel.sink.add(jsonEncode(localIceCandidate.toMap()));
+                break;
+              }
+            },
+            child: Text("Start Video Call"),
+          ),
+          Expanded(child: RTCVideoView(_localRenderer)),
+          Expanded(child: RTCVideoView(_remoteRenderer))
         ],
       ),
     );
--- lib/peer_service.dart
+++ lib/peer_service.dart
@@ -1,10 +1,8 @@
-
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 class PeerService {
   RTCPeerConnection? peer;
   RTCIceCandidate? localIceCandidate;
-
 
   PeerService() {
     initializePeer();
@@ -15,83 +13,56 @@ class PeerService {
       final configuration = {
         'iceServers': [
           {'urls': 'stun:stun.l.google.com:19302'},
-          {'urls': 'stun:stun.l.google.com:5349'},
-          {'urls': 'stun:stun1.l.google.com:3478'},
-          {'urls': 'stun:stun1.l.google.com:5349'},
-          {'urls': 'stun:stun2.l.google.com:19302'},
-          {'urls': 'stun:stun2.l.google.com:5349'},
-          {'urls': 'stun:stun3.l.google.com:3478'},
-          {'urls': 'stun:stun3.l.google.com:5349'},
-          {'urls': 'stun:stun4.l.google.com:19302'},
-          {'urls': 'stun:stun4.l.google.com:5349'},
-          {'urls': 'stun:bn-turn1.xirsys.com'},
           {
             'urls': 'turn:turn.anyfirewall.com:443?transport=tcp',
             'username': 'webrtc',
             'credential': 'webrtc',
-          },
-          {
-            'urls': 'turn:relay1.expressturn.com:3478',
-            'username': 'efRZX7VVZB6250T4HF',
-            'credential': 'UFiJt9Y2Rctg18RL',
-          },
-          {
-            'urls': 'turn:relay1.expressturn.com:3478',
-            'username': 'ef59NGX88DYZX3JU68',
-            'credential': 'Y9JL6obz547h3xa4',
-          },
-          {
-            'username': '0I4QrZu3s-C3mU-256EVtbvg9AnsaItARBnXpye1msFXoJ42E8GXgG1pDeGgAHGBAAAAAGZz2bRTaG9iaGE=',
-            'credential': '721cfd32-2ed6-11ef-8b34-0242ac140004',
-            'urls': [
-              'turn:bn-turn1.xirsys.com:80?transport=udp',
-              'turn:bn-turn1.xirsys.com:3478?transport=udp',
-              'turn:bn-turn1.xirsys.com:80?transport=tcp',
-              'turn:bn-turn1.xirsys.com:3478?transport=tcp',
-              'turns:bn-turn1.xirsys.com:443?transport=tcp',
-              'turns:bn-turn1.xirsys.com:5349?transport=tcp',
-            ],
           },
         ],
         'iceTransportPolicy': 'all',
       };
       peer = await createPeerConnection(configuration);
 
-
       peer!.onIceCandidate = (RTCIceCandidate candidate) {
-        // if(localIceCandidate == null){
-          handleICECandidateEvent(candidate);
-        // }
-
+        localIceCandidate = candidate;
       };
 
       peer!.onIceConnectionState = (RTCIceConnectionState state) {
-        handleICEConnectionStateChangeEvent(state);
+        print('ICE Connection State: $state');
       };
 
       peer!.onTrack = (RTCTrackEvent event) {
-        handleTrackEvent(event);
+        print('Track Event: ${event.track}');
       };
     }
   }
 
-  void handleICECandidateEvent(RTCIceCandidate candidate) {
-    // Handle ICE candidate event
-    print('ICE Candidate: ${candidate.candidate}');
-    localIceCandidate = candidate;
+  Future<RTCSessionDescription?> getAnswer(RTCSessionDescription offer) async {
+    try {
+      await peer!.setRemoteDescription(offer);
+      final answer = await peer!.createAnswer();
+      await peer!.setLocalDescription(answer);
+      return answer;
+    } catch (e) {
+      print('Error getting answer: $e');
+      return null;
+    }
   }
 
-  void handleICEConnectionStateChangeEvent(RTCIceConnectionState state) {
-    // Handle ICE connection state change event
-    print('ICE Connection State: $state');
+  Future<RTCSessionDescription?> getOffer() async {
+    try {
+      final offer = await peer!.createOffer();
+      await peer!.setLocalDescription(offer);
+      return offer;
+    } catch (e) {
+      print('Error getting offer: $e');
+      return null;
+    }
   }
 
-  void handleTrackEvent(RTCTrackEvent event) {
-    // Handle track event
-    print('Track Event: ${event.track}');
+  RTCIceCandidate? getIceCandidate() {
+    return localIceCandidate;
   }
-
-
 
   void closePeer() {
     if (peer != null) {
@@ -99,47 +70,22 @@ class PeerService {
       peer = null;
     }
   }
-
-  Future<RTCSessionDescription?> getAnswer(RTCSessionDescription? offer) async {
-    try {
-      if (peer != null && offer != null) {
-        await peer!.setRemoteDescription(offer);
-        final answer = await peer!.createAnswer();
-        await peer!.setLocalDescription(answer);
-        return answer;
-      }
-    } catch (e) {
-      print('Error getting answer: $e');
-    }
-    return null;
-  }
-
-  Future<void> setLocalDescription(RTCSessionDescription answer) async {
-    try {
-      if (peer != null) {
-        await peer!.setRemoteDescription(answer);
-      }
-    } catch (e) {
-      print('Error setting local description: $e');
-    }
-  }
-
-  Future<RTCSessionDescription?> getOffer() async {
-    try {
-      if (peer != null) {
-        final offer = await peer!.createOffer();
-        await peer!.setLocalDescription(offer);
-        return offer;
-      }
-    } catch (e) {
-      print('Error getting offer: $e');
-    }
-    return null;
-  }
-
-  RTCIceCandidate? getIceCandidate(){
-    return localIceCandidate;
-  }
 }
 
-final peerServiceInstance = PeerService();
+class IceCandidateWrapper {
+  String type;
+  RTCIceCandidate? candidate;
+
+  IceCandidateWrapper({this.type = 'candidate', this.candidate});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'type': type,
+      'candidate': {
+        'candidate': candidate?.candidate,
+        'sdpMid': candidate?.sdpMid,
+        'sdpMLineIndex': candidate?.sdpMLineIndex,
+      }
+    };
+  }
+}
