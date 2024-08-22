3912ae9516da2a1d9f63241a236e716ecae1c8f1
Harikrashna <hparikh@argusoft.com>
2024-08-22 10:41:23 +0530

    Local Camera view added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
@@ -22,38 +23,36 @@ class MyApp extends StatelessWidget {
   }
 }
 
-
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-
-
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>   {
-
+class _HomePageState extends State<HomePage> {
   TextEditingController textEditingController = TextEditingController();
   late WebSocketChannel channel;
   String recieved = "Waiting ====???????";
-
+  final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
 
   @override
-  void initState()  {
+  void initState() {
     connectSocket();
-    channel.stream.listen((message){
+    channel.stream.listen((message) {
       print("=======================>>>>>>>>>>>>>>>> message recieved");
       setState(() {
         recieved = message;
       });
       print(message);
-      print("=======================>>>>>>>>>>>>>>>> ok!!!                                                                              ");
+      print(
+          "=======================>>>>>>>>>>>>>>>> ok!!!                                                                              ");
     });
   }
 
   connectSocket() async {
-    final url = Uri.parse('ws://192.1.150.112:8082/api/videochat/efd6fea9-fa56-4c01-bbe4-0853e40c1866w');
+    final url = Uri.parse(
+        'ws://192.1.150.112:8082/api/videochat/efd6fea9-fa56-4c01-bbe4-0853e40c1866w');
     channel = WebSocketChannel.connect(url);
     print("=======================>>>>>>>>>>>>>>>> channel getting ready");
     await channel.ready;
@@ -61,12 +60,23 @@ class _HomePageState extends State<HomePage>   {
     print("=======================>>>>>>>>>>>>>>>> $channel");
   }
 
+  void initCamera() async {
+    await _localRenderer.initialize();
+    MediaStream stream = await navigator.mediaDevices.getUserMedia({
+      'video': true,
+      'audio': true,
+    });
+
+    setState(() {
+      _localRenderer.srcObject = stream;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           print("=====================");
           print(textEditingController.text);
           channel.sink.add(textEditingController.text);
@@ -83,12 +93,18 @@ class _HomePageState extends State<HomePage>   {
           TextField(
             controller: textEditingController,
           ),
-          Text(recieved)
+          Text(recieved),
+          SizedBox(
+            height: 12,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                initCamera();
+              },
+              child: Text("Start Video call")),
+        Expanded(child: RTCVideoView(_localRenderer))
         ],
       ),
     );
   }
 }
-
-
-
