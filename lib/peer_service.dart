7b4f9f140b69e595ea02ed2001790ac1977d5313
Harikrashna <hparikh@argusoft.com>
2024-08-27 17:42:39 +0530

    offer and ice candidate delay issue resolved

--- lib/peer_service.dart
+++ lib/peer_service.dart
@@ -14,11 +14,45 @@ class PeerService {
     if (peer == null) {
       final configuration = {
         'iceServers': [
-          {'urls': 'stun:stun.l.google.com:19302'},
+          { "urls": 'stun:stun.l.google.com:19302'},
+          { "urls": "stun:stun.l.google.com:19302" },
+          { "urls": "stun:stun.l.google.com:5349" },
+          { "urls": "stun:stun1.l.google.com:3478" },
+          { "urls": "stun:stun1.l.google.com:5349" },
+          { "urls": "stun:stun2.l.google.com:19302" },
+          { "urls": "stun:stun2.l.google.com:5349" },
+          { "urls": "stun:stun3.l.google.com:3478" },
+          { "urls": "stun:stun3.l.google.com:5349" },
+          { "urls": "stun:stun4.l.google.com:19302" },
+          { "urls": "stun:stun4.l.google.com:5349" },
+          { "urls": ["stun:bn-turn1.xirsys.com"] },
           {
-            'urls': 'turn:turn.anyfirewall.com:443?transport=tcp',
-            'username': 'webrtc',
-            'credential': 'webrtc',
+            "urls": "turn:turn.anyfirewall.com:443?transport=tcp",
+            "username": "webrtc",
+            "credential": "webrtc",
+          },
+          {
+            "urls": "turn:relay1.expressturn.com:3478",
+            "username": "efRZX7VVZB6250T4HF",
+            "credential": "UFiJt9Y2Rctg18RL",
+          },
+          {
+            "urls": "turn:relay1.expressturn.com:3478",
+            "username": "ef59NGX88DYZX3JU68",
+            "credential": "Y9JL6obz547h3xa4",
+          },
+          {
+            "username":
+            "0I4QrZu3s-C3mU-256EVtbvg9AnsaItARBnXpye1msFXoJ42E8GXgG1pDeGgAHGBAAAAAGZz2bRTaG9iaGE=",
+            "credential": "721cfd32-2ed6-11ef-8b34-0242ac140004",
+            "urls": [
+              "turn:bn-turn1.xirsys.com:80?transport=udp",
+              "turn:bn-turn1.xirsys.com:3478?transport=udp",
+              "turn:bn-turn1.xirsys.com:80?transport=tcp",
+              "turn:bn-turn1.xirsys.com:3478?transport=tcp",
+              "turns:bn-turn1.xirsys.com:443?transport=tcp",
+              "turns:bn-turn1.xirsys.com:5349?transport=tcp",
+            ],
           },
         ],
         'iceTransportPolicy': 'all',
@@ -33,6 +67,11 @@ class PeerService {
         print('ICE Connection State: $state');
       };
 
+      peer!.onIceGatheringState = (RTCIceGatheringState state){
+        if(state == RTCIceGatheringState.RTCIceGatheringStateComplete){
+          print("and the finallu==========================?>>>>>>>>>>>>>>>>>>>>>>");
+        }
+      };
       peer!.onTrack = (RTCTrackEvent event) {
         print('Track Event: ${event.track}');
       };
@@ -58,6 +97,8 @@ class PeerService {
     try {
       final offer = await peer!.createOffer();
       await peer!.setLocalDescription(offer);
+      // Add a small delay to ensure ICE candidates have time to gather
+      await Future.delayed(Duration(seconds: 2));
       return offer;
     } catch (e) {
       print('Error getting offer: $e');
